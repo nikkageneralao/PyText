6c45a769e9707eff644d242019566be96b32734c
nikkageneralao <nikkapaulinedgeneralao@iskolarngbayan.pup.edu.ph>
2023-02-09 23:45:35 +0800

    Start of modification - added logo and name

--- Window.py
+++ Window.py
@@ -20,7 +20,9 @@ class Window:
         # Initialisation Of window
         self.window = Tk()
         self.window.geometry("1200x700+200+150")
-        self.window.wm_title("Untitled")
+        self.window.wm_title("PyText")
+        p1 = PhotoImage(file='logo.png')
+        self.window.iconphoto(False, p1)
 
         # Initialisation of Text Widget
         self.TextBox = Text(self.window, highlightthickness=0, font=("Helvetica", 14))
